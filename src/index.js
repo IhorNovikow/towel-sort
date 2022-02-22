800863ea05ebd2d41805e0ba6192d7768a9c21a6
IhorNovikow <gosha.noviko2012@gmail.com>
2022-02-22 09:00:41 +0300

    dox:create full task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort (num) {
+  let newNum=[]
+  let newNumTwo=[]
+  num.forEach(Element=>{
+    newNum.push(Element.join(''))
+  })
+  newNum=newNum.join('').split('')
+  newNum.forEach(Element=>{
+    newNumTwo.push(+Element)
+  })
+   newNumTwo.sort(function(a, b) {
+    return a - b;
+  });
+  return (newNumTwo)
