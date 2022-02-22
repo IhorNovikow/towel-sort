04996f9fa8a1bd3db24647eac60e36af799eb79d
IhorNovikow <gosha.noviko2012@gmail.com>
2022-02-22 09:46:53 +0300

    fix: fix mistake with code

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@
 module.exports = function towelSort (num) {
   let newNum=[]
   let a=0
-  num.forEach((Element)=>{
+  if (num==[]){
+  return ([])
+  }else{num.forEach((Element)=>{
   a=a+1
   if (a%2 !=0){  
     console.log(a)
@@ -19,4 +21,5 @@ module.exports = function towelSort (num) {
     }
   })
   return (newNum)
+  }
 }
